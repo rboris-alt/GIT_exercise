a051910bf0b71a91f00e44a12e60a188cd147fb9
BorisRadovanovic <boris.radovanovic@uns.ac.rs>
2022-03-15 08:29:19 +0100

    Init function added

--- main.c
+++ main.c
@@ -2,12 +2,17 @@
 #include <util/delay.h>
 #include <stdint.h>
 
+void ledInit()
+{
+	DDRB |= 1 << 5; // PB5 je izlaz	
+}
+
 int16_t main ()
 {
 	int16_t high_time = 300;
 	int16_t low_time = 700;
 	
-	DDRB |= 1 << 5; // PB5 je izlaz
+    ledInit();
 	
 	while (1)
 	{
